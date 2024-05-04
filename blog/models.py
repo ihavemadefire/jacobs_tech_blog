e45066388891606c33e45e3b99ece1fa332e78c7
Jacob <jacobmeide@gmail.com>
2024-05-04 16:01:08 -0500

    update model, add routing

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from core.utils import unique_slug_generator
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True, null = True, blank = True)
+    sub_title = models.CharField(max_length=200)
+    body = CKEditor5Field('Text', config_name='extends')
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+@receiver(pre_save, sender=Post) 
+def pre_save_receiver(sender, instance, *args, **kwargs): 
+   if not instance.slug: 
+       instance.slug = unique_slug_generator(instance) 
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    ordering = ['-date']
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'blog/post.html'
+    context_object_name = 'post'
