2cbe28e1e764beef9fd195c8aba2a058ac1e8001
Jacob <jacobmeide@gmail.com>
2024-06-23 17:14:39 -0500

    Varius deploy changes

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import TemplateView
 from blog.models import Post
 
-
 class HomePageView(TemplateView):
     template_name = 'home.html'
     context_object_name = 'posts'
